348ae562f2c4bd5961df7bcf70051941a95ee1b0
malak119 <M_malak-2014@hotmail.com>
2021-08-09 19:55:23 +0300

    adding google fonts to /codeWars.html file, changing the home icon and give it a style: font-size, linking codeWars.css file with /codeWars.html. Also, creating /codeWars.css file to set a properties for the elements in html and JavaScript file, creting media query for tablets and laptops  and change some behavior in /codeWars.js file by declaring new variable (container) which contains all the boxes, append it to its father(main) and addingg innerHTML to the elements which were createdRelats #6

--- codeWarsPage/codeWars.js
+++ codeWarsPage/codeWars.js
@@ -28,34 +28,34 @@ challenges.forEach((reach) => {
       // creating a main box that the data are rendered in
       const box = document.createElement("div");
       box.setAttribute("class", "content");
-    // creating the first key and assign it to variable called name, have a class = "name"
+      // creating the first key and assign it to variable called name, have a class = "name"
       const name = document.createElement("h1");
       name.setAttribute("class", "name");
       name.textContent = "Challenge Name: " + data.name;
-      // creating the second key and assign it to variable called Category, have a class = "Category"   
+      // creating the second key and assign it to variable called Category, have a class = "Category"
       const Category = document.createElement("h5");
       Category.setAttribute("class", "category");
       Category.textContent = data.category;
-      Category.innerHTML = `Challenge Category: `+ data.category;
+      Category.innerHTML = `Challenge Category: ` + data.category;
 
-    // creating the third key and assign it to variable called Kata, have a class = "kata"  
+      // creating the third key and assign it to variable called Kata, have a class = "kata"
       const Kata = document.createElement("h5");
       Kata.setAttribute("class", "kata");
       Kata.textContent = data.rank.name;
-      Kata.innerHTML = `Kata number: `+ data.rank.name;
+      Kata.innerHTML = `Kata number: ` + data.rank.name;
 
-    // creating the forth key and assign it to variable called link, have a class = "link"  
-    // create varaible called url, which take the url from api and save it on
+      // creating the fourth key and assign it to variable called link, have a class = "link"
+      // create varaible called url, which take the url from api and save it on
       const url = data.url;
       const link = document.createElement("a");
       link.setAttribute("class", "link");
       link.innerHTML = `Challenge link: <b><a href = "${url}" >Click Here</a></b>`;
       console.log(url);
 
-    // appending childes to its fathers
-    // father.appendChild(child)
-     main.appendChild(container);
-     container.appendChild(box);
+      // appending childes to its fathers
+      // father.appendChild(child)
+      main.appendChild(container);
+      container.appendChild(box);
       box.appendChild(name);
       box.appendChild(Category);
       box.appendChild(Kata);
