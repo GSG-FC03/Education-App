fae6b5240f243e44d410b3c2b7a93563cfe3ee08
Heba-Darabeh <hebadarabeh02@gmail.com>
2021-08-10 22:11:19 +0300

    hide the signin elements and show the welcome message and display the username >Relates #3

--- signInGuestPage/signIn.js
+++ signInGuestPage/signIn.js
@@ -1,16 +1,30 @@
-const loginUser = (e = window.event) => {
+const loginSection = document.getElementById("login-section");
+const loginButton = document.getElementById("login-button");
+const welcomeSection = document.getElementById("welcome");
+
+loginButton.addEventListener("click", (e) => {
   e.preventDefault();
   const usernameInput = document.getElementById("username");
   const userTypeInput = document.getElementById("userType");
-
-  if (usernameInput.value === "" || userTypeInput.value === "") {
+  const username = usernameInput?.value || "";
+  const userType = userTypeInput?.value || "";
+  if (!username) {
     alert("Please inter your username!");
+    return;
   }
-
+  if (!userType) {
+    alert("Please inter your user type!");
+    return;
+  }
   const user = {
-    username: usernameInput.value,
-    userType: userTypeInput.value,
+    username,
+    userType,
   };
 
-  window.localStorage.setItem("user", JSON.stringify(user));
-};
+  localStorage.setItem("user", JSON.stringify(user));
+  const welcomeMessage_h2 = document.getElementById("user-name");
+  const welcomeMessage = welcomeMessage_h2?.innerHTML || "";
+  welcomeMessage_h2.innerHTML = `${welcomeMessage} ${username}!`;
+  loginSection.style.display = "none";
+  welcomeSection.style.display = "block";
+});
